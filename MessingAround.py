feb1e4b0506c36a1eaabc83fff43c998ebd26c50
Matthew Zimra Yedija Silaban <matthew.yedija@gmail.com>
2026-05-01 00:14:04 +0800

    try out panda plotting techniques

---
+++ MessingAround.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+try:
+    df = pd.read_csv("Teen_Mental_Health_Dataset.csv")
+    #print(df.describe())
+    #gender_ave_st = df.groupby("gender")["daily_social_media_hours"].mean()
+    #print(gender_ave_st)
+    gender_based = df.groupby("gender")["daily_social_media_hours"].mean()
+    gender_based.plot(title="Gender-Screentime Graph", kind="bar")
+    plt.xticks(rotation=45)
+    plt.show()
+    
+    
+
+
+except Exception as e:
+    print(f"Error fetching data: {e}")
+
