04ef0f40d65bb05e3c7457ba2077873e1301fd70
Matthew Zimra Yedija Silaban <matthew.yedija@gmail.com>
2026-05-01 00:13:45 +0800

    implement clean_data() and load_data()

---
+++ DataProcessor.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import os
+import yfinance as yf
+
+class DataProcessor:
+    def __init__(self):
+        self.df = None  
+
+    def load_data(self, file_path):
+        extension = os.path.splitext(file_path)[1].lower()
+
+        try:
+            if extension == ".csv":
+                self.df = pd.read_csv(file_path)
+            elif extension in [".xls", ".xlsx"]:
+                self.df = pd.read_excel(file_path)
+            elif extension == ".json":
+                self.df = pd.read_json(file_path)
+            else:
+                return f"Unsupported file format: {extension}"
+            return f"Successfully loaded {len(self.df)} rows and {len(self.df.columns)} columns."
+        except Exception as e:
+            return f"Error loading file: {e}"
+        
+    def clean_data(self, drop_threshold=0.5):
+        if self.df is None:
+            return "No data loaded."
+        
+        # remove duplicates
+        initial_count = len(self.df)
+        self.df.drop_duplicates(inplace=True)
+        duplicates_removed = initial_count - len(self.df)
+
+        # drops columns that are mostly empty (> 50%)
+        limit = int(len(self.df) * drop_threshold)
+        self.df.dropna(thresh=limit, axis=1, inplace=True)
+
+        number_cols = self.df.select_dtypes(include=["number"]).columns
+        self.df["number_cols"] = self.df["number_cols"].fillna(self.df["number_cols"].mean())
+
+        object_cols = self.df.select_dtypes(include=["object"]).columns
+        self.df["object_cols"] = self.df["object_cols"].fillna("Unknown.")
+
+        dropped_count = initial_count - duplicates_removed - len(self.df)
+
+        return f"Cleaned! Removed {duplicates_removed} duplicates and dropped {dropped_count}"
+    
+    def visualise_bar(self, *instances):
+        """ Plots bar graphs to compare single values across time, categories or instances. """
+        if self.df is None:
+            return "No data for graphing."
+        
+        self.df.plot(kind="bar")
+    
+        
